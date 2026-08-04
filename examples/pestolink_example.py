0d59eb16ce8e29f0bce818f8b8425ca956f4182f
Carter Perun <carterperun@icloud.com>
2026-08-04 16:44:55 -0400

    cleanup after mess

--- examples/pestolink_example.py
+++ examples/pestolink_example.py
@@ -1,63 +1,31 @@
 # Import necessary modules
 from machine import Pin, ADC
-import bluetooth
 import time
 import math
 
 from XRPLib.defaults import *
 from pestolink import PestoLinkAgent
 
-#Choose the name your robot shows up as in the Bluetooth paring menu
-#Name should be 8 characters max!
-robot_name = "T3amThr3"
+# robot name, bluetooth broadcasts this
+robot_name = "H3ALTHY"
 
 # Create an instance of the PestoLinkAgent class
 pestolink = PestoLinkAgent(robot_name)
 
-def deadzone(value, threshold=0.1):
-    if abs(value) < threshold:
-        return 0.0
-    return value
 
-def performChallenge1():
+# drive 30 cm straight using library
+def testStraightDrive():
+    drivetrain.straight(30)
     return True
 
-def performChallenge2():
-    return True
-
-def performChallenge3():
-    return True
-
-def performChallenge4():
-    return True
-
-def performChallenge5():
-    return True
-
-def performChallenge6():
-    return True
-
-def performChallenge7():
-    return True
-
-def performChallenge8():
-    return True
-
-def performChallenge9():
-    return True
-
-def performChallenge10():
-    return True
-
-def performChallenge11():
-    return True
-
-# Start an infinite loop
 while True:
-    if pestolink.is_connected():  # Check if a BLE connection is established
-        rotation = deadzone(-1 * pestolink.get_axis(0))
-        throttle = deadzone(-1 * pestolink.get_axis(1))
-        
+    if pestolink.is_connected():  
+        rotation = -1 * pestolink.get_axis(0)
+        throttle = -1 * pestolink.get_axis(1)
+
+        if(pestolink.get_button(1)):
+            testStraightDrive()
+
         drivetrain.arcade(throttle, rotation)
         
         if(pestolink.get_button(0)):
@@ -68,7 +36,41 @@ while True:
         batteryVoltage = (ADC(Pin("BOARD_VIN_MEASURE")).read_u16())/(1024*64/14)
         pestolink.telemetryPrintBatteryVoltage(batteryVoltage)
 
-    else: #default behavior when no BLE connection is open
+    else: 
         drivetrain.arcade(0, 0)
+
         servo_one.set_angle(70)
 
+
+def performChallengeMaze():
+    return True
+
+def performChallengeTransporter():
+    return True
+
+def performChallengePatternMatch():
+    return True
+
+def performChallengeStacker():
+    return True
+
+def performChallengeMiniGolf():
+    return True
+
+def performChallengeSumo():
+    return True
+
+def performChallengeTowerOfHanoi():
+    return True
+
+def performChallengeBasketGallery():
+    return True
+
+def performChallengeTowerSmash():
+    return True
+
+def performChallengeLineFollower():
+    return True
+
+def performChallengeBallSearch():
+    return True
