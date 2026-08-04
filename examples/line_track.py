ef4b35a7b6871228dcba4fd2a11564c00c59e672
Austin Tleon <austintleon@dhcp-vl2043-6797.cornell-visitor.cit.cornell.edu>
2026-08-04 17:39:07 -0400

    Update line track script

--- examples/line_track.py
+++ examples/line_track.py
@@ -1,19 +1,21 @@
 from XRPLib.defaults import *
 import time
 
-def line_track():
-    # Set base effort value
-    base_effort = 0.3
-    # Set KP value
-    KP = 0.6
-    # Put in while loop so it runs infinitely
-    while True:
-        # Calculate error (fixed hyphen to dot)
-        error = reflectance.get_left() - reflectance.get_right()
-        # Tell drivetrain to set effort values on right and left motors based on error
-        drivetrain.set_effort(base_effort - error * KP, base_effort + error * KP)
-        # Re-calculate values every 0.01 seconds
-        time.sleep(0.01)
+base_effort = 0.25
+KP = 0.6
+
+while True:
+    left = reflectance.get_left()
+    right = reflectance.get_right()
+
+    error = right - left
+
+    print(f"L={left:.3f} R={right:.3f} error={error:+.3f}")
+    drivetrain.set_effort(
+        base_effort - error * KP,
+        base_effort + error * KP
+    )
+    time.sleep(0.01)
 
 # Run function here
 line_track()
