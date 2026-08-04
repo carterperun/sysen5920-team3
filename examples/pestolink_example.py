34586947e511903d515dc388c0065f4bab6ddba5
Carter Perun <carterperun@icloud.com>
2026-08-03 20:45:09 -0400

    adding deadzone for motor testing

--- examples/pestolink_example.py
+++ examples/pestolink_example.py
@@ -9,16 +9,21 @@ from pestolink import PestoLinkAgent
 
 #Choose the name your robot shows up as in the Bluetooth paring menu
 #Name should be 8 characters max!
-robot_name = "XRProbot"
+robot_name = "T3amThr3"
 
 # Create an instance of the PestoLinkAgent class
 pestolink = PestoLinkAgent(robot_name)
 
+def deadzone(value, threshold=0.1):
+    if abs(value) < threshold:
+        return 0.0
+    return value
+
 # Start an infinite loop
 while True:
     if pestolink.is_connected():  # Check if a BLE connection is established
-        rotation = -1 * pestolink.get_axis(0)
-        throttle = -1 * pestolink.get_axis(1)
+        rotation = deadzone(-1 * pestolink.get_axis(0))
+        throttle = deadzone(-1 * pestolink.get_axis(1))
         
         drivetrain.arcade(throttle, rotation)
         
